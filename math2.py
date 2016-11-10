c7bf12033a3d645d4d49d791880463573a491a7e
Mateus Lucas <mateuslucasxcrsilveira@hotmail.com>
2016-11-10 17:07:18 -0200

    Add files via upload

---
+++ math2.py
@@ -0,0 +1,13 @@
+# -*- coding: latin1 -*-
+import random
+import string
+# Escolha uma letra
+print random.choice(string.ascii_letters)
+# Escolha um número de 1 a 10
+print random.randrange(1, 11)
+# Escolha um float no intervalo de 0 a 1
+print random.random()
+a = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+'S','T','U','V','X','W','Y','Z','a','b','c','d','e','f','g','h','i','j','k',
+'l','m','n','o','p','q','r','s','t','u','v','x','w','y','z','1','2','3','4',
+'5','6','7','8','9','0','!','@','#','$','%','&','*','ç']
---
+++ senha.py
@@ -0,0 +1,7 @@
+import random
+import string
+a = []
+for i in range(0,16):
+    a.append(random.choice(string.letters))
+    
+print a
